a448cc0b6d23b9a93591b57675a81cba4fca139b
sethusaim <sethusaim@gmail.com>
2023-01-23 10:16:47 +0530

    updates

--- scripts/eks_cluster.sh
+++ scripts/eks_cluster.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-eksctl create cluster --name kubeflow --version 1.23 --region us-east-1 --zones us-east-1a,us-east-1b --nodegroup-name kubeflow-nodes --node-type t2.large --nodes-min 2 --nodes-max 4 --with-oidc
+eksctl create cluster --name kubeflow --version 1.22 --region us-east-1 --zones us-east-1a,us-east-1b --nodegroup-name kubeflow-nodes --node-type t2.large --nodes-min 2 --nodes-max 4 --with-oidc
