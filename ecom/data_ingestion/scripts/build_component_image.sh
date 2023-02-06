7f7cf153ad6acb976450daaaca663dbe4a2f9794
sethusaim <sethusaim@gmail.com>
2023-02-06 18:04:50 +0530

    removed

--- ecom/data_ingestion/scripts/build_component_image.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-docker build -t 566373416292.dkr.ecr.us-east-1.amazonaws.com/ecom-data-ingestion:latest .
-          
-docker push 566373416292.dkr.ecr.us-east-1.amazonaws.com/ecom-data-ingestion:latest
