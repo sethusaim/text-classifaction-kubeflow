3f5366a1d0e85ebdb5eb44d67c623fcc69eace2f
sethusaim <sethusaim@gmail.com>
2023-02-17 15:00:24 +0530

    added data transformation component

---
+++ ecom/data_transformation/src/cloud_storage/aws_operation.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+import boto3
+
+from src.exception import EcomException
+
+
+class S3Operation:
+    def sync_folder_to_s3(
+        self, folder: str, bucket_name: str, bucket_folder_name: str
+    ) -> None:
+        try:
+            os.system(f"aws s3 sync {folder} s3://{bucket_name}/{bucket_folder_name}/ ")
+
+        except Exception as e:
+            raise EcomException(e, sys)
+
+    def sync_folder_from_s3(
+        self, folder: str, bucket_name: str, bucket_folder_name: str
+    ) -> None:
+        try:
+            os.system(f"aws s3 sync s3://{bucket_name}/{bucket_folder_name}/ {folder} ")
+
+        except Exception as e:
+            raise EcomException(e, sys)
+
+    def get_pipeline_artifacts(self, bucket_name: str) -> None:
+        try:
+            s3_client = boto3.client("s3")
+
+            response = s3_client.list_objects_v2(
+                Bucket=bucket_name, Prefix="artifacts/"
+            )["Contents"]
+
+            latest = max(response, key=lambda x: x["LastModified"])["Key"]
+
+            artifacts_dir = "/".join(latest.split("/")[:2])
+
+            self.sync_folder_from_s3(
+                folder=artifacts_dir,
+                bucket_name=bucket_name,
+                bucket_folder_name=artifacts_dir,
+            )
+
+            return artifacts_dir.split("/")[1]
+
+        except Exception as e:
+            raise EcomException(e, sys)
