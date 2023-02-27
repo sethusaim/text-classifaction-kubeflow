92e28d46200cba2560bf3334e22d0915a595728f
sethusaim <sethusaim@gmail.com>
2023-02-27 16:23:57 +0530

    minor change

--- ecom/model_evaluation/run.py
+++ ecom/model_evaluation/run.py
@@ -24,6 +24,13 @@ def start_model_evaluation():
     except Exception as e:
         raise EcomException(e, sys)
 
+    finally:
+        s3.sync_folder_to_s3(
+            folder=tp.artifacts_dir,
+            bucket_name=tp.artifact_bucket_name,
+            bucket_folder_name=tp.artifacts_dir,
+        )
+
 
 if __name__ == "__main__":
     start_model_evaluation()
