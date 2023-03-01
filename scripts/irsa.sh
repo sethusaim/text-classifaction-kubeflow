a66f15e04bfce82d0ed4cf9c8494984d5f09ed87
sethusaim <sethusaim@gmail.com>
2023-03-01 13:32:03 +0530

    added scripts for components

---
+++ scripts/irsa.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+# arn:aws:iam::566373416292:policy/ecom_kubeflow_policy Create this policy with s3 and ecr full access
+
+eksctl create iamserviceaccount --name kube-ecom-sa --cluster kubeflow --namespace kubeflow-user-example-com --role-name kube_ecom_role --attach-policy-arn=arn:aws:iam::566373416292:policy/ecom_kubeflow_policy --approve --override-existing-serviceaccounts
--- train.py
+++ train.py
@@ -1,37 +1,62 @@
 import os
 
 from kfp import Client
+from kfp.compiler import Compiler
 from kfp.components import load_component_from_file
-from kfp.dsl import pipeline, BaseOp
+from kfp.dsl import BaseOp, pipeline
 
 from ecom.api.auth import get_istio_auth_session
 
-KUBEFLOW_ENDPOINT = os.environ["KUBEFLOW_ENDPOINT"]
-KUBEFLOW_USERNAME = os.environ["KUBEFLOW_USERNAME"]
-KUBEFLOW_PASSWORD = os.environ["KUBEFLOW_PASSWORD"]
+# KUBEFLOW_ENDPOINT = os.environ["KUBEFLOW_ENDPOINT"]
+# KUBEFLOW_USERNAME = os.environ["KUBEFLOW_USERNAME"]
+# KUBEFLOW_PASSWORD = os.environ["KUBEFLOW_PASSWORD"]
 
 
-auth_session = get_istio_auth_session(
-    url=KUBEFLOW_ENDPOINT, username=KUBEFLOW_USERNAME, password=KUBEFLOW_PASSWORD
-)
+# auth_session = get_istio_auth_session(
+#     url=KUBEFLOW_ENDPOINT, username=KUBEFLOW_USERNAME, password=KUBEFLOW_PASSWORD
+# )
 
 data_ingestion = load_component_from_file("kfp_components/data_ingestion.yaml")
+
+data_validation = load_component_from_file("kfp_components/data_validation.yaml")
+
+data_transformation = load_component_from_file(
+    "kfp_components/data_transformation.yaml"
+)
+
+model_training = load_component_from_file("kfp_components/model_training.yaml")
 
 
 @pipeline(name="Train Pipeline")
 def train_pipeline():
     task_1: BaseOp = data_ingestion()
 
+    task_2: BaseOp = data_validation()
+
+    task_2.after(task_1)
+
+    # task_3: BaseOp = data_transformation()
+
+    # task_3.after(task_2)
+
+    # task_4: BaseOp = model_training()
+
+    # task_4.after(task_3)
+
 
 if __name__ == "__main__":
-    client = Client(
-        host=f"{KUBEFLOW_ENDPOINT}/pipeline", cookies=auth_session["session_cookie"]
+    Compiler().compile(
+        pipeline_func=train_pipeline, package_path="training-pipeline.yaml"
     )
 
-    client.create_run_from_pipeline_func(
-        pipeline_func=train_pipeline,
-        arguments={},
-        namespace="kubeflow-user-example-com",
-        experiment_name="kube-s3-test",
-        service_account="kube-s3-sa",
-    )
+    # client = Client(
+    #     host=f"{KUBEFLOW_ENDPOINT}/pipeline", cookies=auth_session["session_cookie"]
+    # )
+
+    # client.create_run_from_pipeline_func(
+    #     pipeline_func=train_pipeline,
+    #     arguments={},
+    #     namespace="kubeflow-user-example-com",
+    #     experiment_name="kube-s3-test",
+    #     service_account="kube-s3-sa",
+    # )
