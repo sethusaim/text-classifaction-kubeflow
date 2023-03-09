0ad1b65386a8f08932013b2311e8dc89ab216388
sethusaim <sethusaim@gmail.com>
2023-03-09 14:52:05 +0530

    added data ingestion component

--- scripts/build_data_ingestion_component.sh
+++ scripts/build_data_ingestion_component.sh
@@ -1,9 +1,7 @@
 #!bin/bash
 
-mongodb_url=$1
-
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 566373416292.dkr.ecr.us-east-1.amazonaws.com
 
-docker build --build-arg MONGO_DB_URL=$mongodb_url -t 566373416292.dkr.ecr.us-east-1.amazonaws.com/ecom_data_ingestion:latest ecom/data_ingestion/
+docker build -t 566373416292.dkr.ecr.us-east-1.amazonaws.com/ecom_data_ingestion:latest ecom/data_ingestion/
 
 docker push 566373416292.dkr.ecr.us-east-1.amazonaws.com/ecom_data_ingestion:latest
