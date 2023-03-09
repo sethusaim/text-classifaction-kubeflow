66f0eb3269e5dcfcbde32ee6b842fbe0c6ce9f7c
sethusaim <sethusaim@gmail.com>
2023-03-09 11:49:28 +0530

    removed infrastructure folder

--- infrastructure/ecom_artifacts/artifacts.tf
+++
@@ -1,9 +0,0 @@
-resource "random_integer" "random" {
-  min = 1
-  max = 50000
-}
-
-resource "aws_s3_bucket" "artifacts_bucket" {
-  bucket        = "${random_integer.random.id}${var.artifacts_bucket_name}"
-  force_destroy = var.force_destroy_bucket
-}
