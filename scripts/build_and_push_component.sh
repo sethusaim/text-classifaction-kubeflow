97c6949e90479229620d49962821b50bc2d33eb3
sethusaim <sethusaim@gmail.com>
2023-03-15 12:17:45 +0530

    minor change

--- scripts/build_and_push_component.sh
+++ scripts/build_and_push_component.sh
@@ -6,8 +6,8 @@ repo=$2
 
 build_number=$3
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 566373416292.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 347460842118.dkr.ecr.us-east-1.amazonaws.com
 
-docker build -t 566373416292.dkr.ecr.us-east-1.amazonaws.com/$repo:$build_number $path/
+docker build -t 347460842118.dkr.ecr.us-east-1.amazonaws.com/$repo:$build_number $path/
 
-docker push 566373416292.dkr.ecr.us-east-1.amazonaws.com/$repo:$build_number
+docker push 347460842118.dkr.ecr.us-east-1.amazonaws.com/$repo:$build_number
--- scripts/irsa.sh
+++ scripts/irsa.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-# arn:aws:iam::566373416292:policy/ecom_kubeflow_policy - Create this policy with s3 and ecr full access
+# arn:aws:iam::347460842118:policy/ecom_kubeflow_policy - Create this policy with s3 and ecr full access
 
-eksctl create iamserviceaccount --name kube-ecom-sa --cluster kubeflow --namespace kubeflow-user-example-com --role-name kube_ecom_role --attach-policy-arn=arn:aws:iam::566373416292:policy/ecom_kubeflow_policy --approve --override-existing-serviceaccounts
+eksctl create iamserviceaccount --name kube-ecom-sa --cluster kubeflow --namespace kubeflow-user-example-com --role-name kube_ecom_role --attach-policy-arn=arn:aws:iam::347460842118:policy/ecom_kubeflow_policy --approve --override-existing-serviceaccounts
 
 kubectl create clusterrolebinding kube-ecom-sa-cluster-role-binding --clusterrole=admin --serviceaccount=kubeflow-user-example-com:kube-ecom-sa
