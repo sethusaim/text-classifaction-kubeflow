f8a35bd3fb9ebf3198886faa11ae65cc220e7dbc
sethusaim <sethusaim@gmail.com>
2023-04-20 12:23:36 +0530

    added scripts to get dashboard url

--- scripts/eks_cluster.sh
+++ scripts/eks_cluster.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 
 eksctl create cluster --name tekton --version 1.22 --region us-east-1 --zones us-east-1a,us-east-1b --nodegroup-name tekton-nodes --node-type t2.large --nodes-min 2 --nodes-max 4 --with-oidc
+
+kubectl patch svc istio-ingressgateway -n istio-system -p '{"spec":{"type":"LoadBalancer"}}'
+
+kubectl get svc istio-ingressgateway -n istio-system 
